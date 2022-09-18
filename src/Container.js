bba3e08be669549fb0ab26c18f2418463f414e62
guilherme <grgomes82@gmail.com>
2022-09-18 19:23:05 -0300

    second commit

--- src/Container.js
+++ src/Container.js
@@ -40,24 +40,61 @@ function Stories() {
 
   )
 }
-function Feed() {
-  const [isActive, setIsActive] = React.useState("");
-  const toggle = (index) => {
-    if(isActive === index){
-      setIsActive(null)
+function Feed(props) {
+  const [isActivePost, setIsActivePost] = React.useState(null);
+  const [isActiveLike, setIsActiveLike] = React.useState(null);
+  const [contadorpost, setContadorPost] = React.useState(101369);
+  
+
+
+
+  let handlePost = (index) => {
+    if(isActivePost === index){
+      setIsActivePost(null)
     } else {
-      setIsActive(index)
+      setIsActivePost(index)
+
     }
   }
+  let handleLike = (indice) => {
+    if(isActiveLike === indice){
+      setIsActiveLike(null);
+      setContadorPost(contadorpost - 1);
 
+    } else {
+      setIsActiveLike(indice);
+      setContadorPost(contadorpost + 1);
+
+    }
+  }
   
+  // const toggle = (index) => {
+  //   setIsActive((prev) => {
+  //     return prev === index ? null : index
+  //   });
+  // }
+  // className={isActive === index ? 'ioncolor' : ''} onClick={() => toggle(index)} >
+
+  // function handleClick (){
+  //   isActive === '' ? setIsActive('ioncolor') : setIsActive('');
+  // }
+  // const handleClick = index => {
+  //   setIsActive(index); 
+  // };
+  // const handleClick = (i) => {
+  //   if (isActive.indexOf(i) === -1) {
+  //     setIsActive([...isActive, i]);
+  //   }
+  // };
+
+
 const posts = [
-    {id:1, imagepost: "gato-telefone.svg", imagefeed:"meowed.svg",imagecoment:"respondeai.svg"},
-    {id:2, imagepost: "dog.svg", imagefeed:"barked.svg",imagecoment:"adorable_animals.svg"},
+    {id:1, imagepost: "gato-telefone.svg", imagefeed:"meowed.svg",imagecoment:"respondeai.svg",quantidade: 101523},
+    {id:2, imagepost: "dog.svg", imagefeed:"barked.svg",imagecoment:"adorable_animals.svg", quantidade: 99159}
   ]
   const componentFeed =
   posts.map(
-    (post, i) => 
+    (post,i) => 
     <div class="post" >
         <div class="post-title">
           <div class="persona">
@@ -76,17 +113,19 @@ const posts = [
         <div class="post-action">
           <div class="esquerda">
             <ion-icon name="heart-outline"></ion-icon>
-            <ion-icon name="chatbubble-outline"></ion-icon>
+            <div key={i} className={isActiveLike === i ? 'ioncolorlike': ''} onClick={() => handleLike(i)}>
+              <ion-icon name="chatbubble-outline"></ion-icon>
+            </div>
             <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
-          <div class="direita" className={isActive === i ? 'ioncolor' : ''} key={i} onClick={() => toggle(i)} >
+          <div class="direita" key={i} className={isActivePost === i ? 'ioncolorpost': ''} onClick={() => handlePost(i)} >
             <ion-icon name="bookmark-outline" ></ion-icon>          
           </div>
         </div>
         <div class="post-coments">
           <img class="profile post-coments-img"
             src={post.imagecoment} />
-          <p>Curtido por respondeai e outras 101.523 pessoas</p>
+          <p>Curtido por respondeai e outras {contadorpost} pessoas</p>
         </div>
       </div>
   )
